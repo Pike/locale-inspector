19a43d863f655888c14f4992541178df997c78dc
Axel Hecht <axel@pike.org>
2013-04-29 16:08:07 +0200

    (re-)license to MPL2

--- l10ninsp/scheduler.py
+++ l10ninsp/scheduler.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from twisted.python import log
 from buildbot.scheduler import BaseUpstreamScheduler
 from buildbot.sourcestamp import SourceStamp
